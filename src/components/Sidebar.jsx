1918f067fdecd6ff36055d2ef98857e9da19c20b
Adistian Herlambang <adistian59@gmail.com>
2025-12-17 00:46:40 +0700

    ya

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,13 @@
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { userStore } from "../state/state";
 
 // eslint-disable-next-line react/prop-types
 export default function Sidebar({ closeMobileMenu }) {
   const location = useLocation();
   const isActive = (path) => location.pathname === path;
+  const logout = userStore((state) => state.logout);
+
+  const navigate = useNavigate();
 
   // Class helper untuk item menu
   const getLinkClass = (path) => `
@@ -14,6 +18,11 @@ export default function Sidebar({ closeMobileMenu }) {
         : "text-slate-800 hover:bg-[#773FF9] hover:text-white"
     }
   `;
+
+  const handleLogout = () => {
+    logout()
+    navigate("/")
+  }
 
   return (
     <div className="h-full w-50 lg:h-58 flex flex-col p-4 overflow-y-auto lg:border rounded-2xl">
@@ -48,12 +57,11 @@ export default function Sidebar({ closeMobileMenu }) {
         </Link>
       </nav>
 
-      <Link
-        to="/"
+      <div onClick={handleLogout}
         className="flex items-center px-4 py-2 text-red-400 hover:bg-slate-800 hover:text-red-300 rounded-lg transition-colors lg:hidden"
       >
         Keluar
-      </Link>
+      </div>
     </div>
   );
 }
